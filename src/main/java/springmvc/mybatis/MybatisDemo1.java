20e51ea2e02b267233a0a988f006e7837155bb90
fhp45 <948592025@qq.com>
2018-09-12 20:41:19 +0800

    add mybatis demo1
    使用方式1：使用SqlSession实例直接来执行在User.xml文件中映射的SQL语句。

---
+++ src/main/java/springmvc/mybatis/MybatisDemo1.java
@@ -0,0 +1,45 @@
+package springmvc.mybatis;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import springmvc.model.User;
+
+public class MybatisDemo1 {
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader;
+
+	static {
+		try {
+			reader = Resources.getResourceAsReader("mybatis/MybatisCfg.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static SqlSessionFactory getSession() {
+		return sqlSessionFactory;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SqlSession session = sqlSessionFactory.openSession();
+		try {
+			User user = (User) session.selectOne(
+					"springmvc.model.UserMapper.GetUserByID", 1);
+			if(user!=null){
+				String userInfo = "名字："+user.getName()+", 年龄："+user.getName()+", 住址："+user.getLocation();
+				System.out.println(userInfo);
+			}
+		} finally {
+			session.close();
+		}
+	}
+
+}
